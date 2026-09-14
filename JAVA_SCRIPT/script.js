95f9ea7d3961042fd4f8087dab66f9b1810d3998
ajaygit-prog <ajayprakashmishra1001@gmail.com>
2026-09-14 23:03:45 +0530

    first js program

--- JAVA_SCRIPT/script.js
+++ JAVA_SCRIPT/script.js
@@ -1,2 +1,11 @@
 console.log("first message") ;
 console.log("i will learn you also do not worry")
+
+const fun = ()=> {
+    console.log("I am callback");
+}
+
+const funct = ()=>{
+    fun() ;
+}
+funct() ;
